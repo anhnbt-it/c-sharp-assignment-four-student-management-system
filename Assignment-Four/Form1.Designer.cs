3ff8cb1e3069cda91423ec3bd185b6bbdcf397f1
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-04-26 21:20:24 +0700

    Initial commit

---
+++ Assignment-Four/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Assignment_Four
+{
+    partial class StudentManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudents = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cboStudents = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.pictAvatar = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lstAvailabel = new System.Windows.Forms.ListBox();
+            this.lstSelected = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStudents
+            // 
+            this.lblStudents.AutoSize = true;
+            this.lblStudents.Location = new System.Drawing.Point(12, 23);
+            this.lblStudents.Name = "lblStudents";
+            this.lblStudents.Size = new System.Drawing.Size(49, 13);
+            this.lblStudents.TabIndex = 0;
+            this.lblStudents.Text = "Students";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 52);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 82);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Age";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 113);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(12, 145);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Available";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(212, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Selected";
+            // 
+            // cboStudents
+            // 
+            this.cboStudents.FormattingEnabled = true;
+            this.cboStudents.Location = new System.Drawing.Point(86, 20);
+            this.cboStudents.Name = "cboStudents";
+            this.cboStudents.Size = new System.Drawing.Size(180, 21);
+            this.cboStudents.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(86, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 20);
+            this.txtName.TabIndex = 8;
+            this.txtName.Validated += new System.EventHandler(this.txtName_Validated);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(86, 110);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 20);
+            this.txtEmail.TabIndex = 9;
+            this.txtEmail.Validated += new System.EventHandler(this.txtEmail_Validated);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(86, 140);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(180, 20);
+            this.txtPhone.TabIndex = 10;
+            this.txtPhone.Validated += new System.EventHandler(this.txtPhone_Validated);
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(86, 80);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(180, 20);
+            this.nudAge.TabIndex = 11;
+            this.nudAge.Validated += new System.EventHandler(this.nudAge_Validated);
+            // 
+            // pictAvatar
+            // 
+            this.pictAvatar.Location = new System.Drawing.Point(272, 20);
+            this.pictAvatar.Name = "pictAvatar";
+            this.pictAvatar.Size = new System.Drawing.Size(100, 100);
+            this.pictAvatar.TabIndex = 12;
+            this.pictAvatar.TabStop = false;
+            this.pictAvatar.Validated += new System.EventHandler(this.pictAvatar_Validated);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(285, 139);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 13;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lstAvailabel
+            // 
+            this.lstAvailabel.FormattingEnabled = true;
+            this.lstAvailabel.Location = new System.Drawing.Point(48, 193);
+            this.lstAvailabel.Name = "lstAvailabel";
+            this.lstAvailabel.Size = new System.Drawing.Size(120, 121);
+            this.lstAvailabel.TabIndex = 14;
+            // 
+            // lstSelected
+            // 
+            this.lstSelected.FormattingEnabled = true;
+            this.lstSelected.Location = new System.Drawing.Point(215, 193);
+            this.lstSelected.Name = "lstSelected";
+            this.lstSelected.Size = new System.Drawing.Size(120, 121);
+            this.lstSelected.TabIndex = 15;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(174, 222);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(35, 23);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = ">";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(174, 251);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(35, 23);
+            this.btnRemove.TabIndex = 17;
+            this.btnRemove.Text = "<";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(20, 326);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 18;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(108, 326);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 19;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(200, 326);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(285, 326);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 21;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // StudentManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstSelected);
+            this.Controls.Add(this.lstAvailabel);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.pictAvatar);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.cboStudents);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblStudents);
+            this.Name = "StudentManage";
+            this.Text = "Student Manager";
+            this.Load += new System.EventHandler(this.StudentManage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudents;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cboStudents;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.NumericUpDown nudAge;
+        private System.Windows.Forms.PictureBox pictAvatar;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lstAvailabel;
+        private System.Windows.Forms.ListBox lstSelected;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Assignment-Four/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Assignment_Four
+{
+    public partial class StudentManage : Form
+    {
+
+        private string connectionString = "SERVER=LAB35\\SQL2012;DataBase=C0501L_WinForm;uid=lab;pwd=";
+        private ErrorProvider nameErrorProvider;
+        private ErrorProvider ageErrorProvider;
+        private ErrorProvider emailErrorProvider;
+        private ErrorProvider phoneErrorProvider;
+        private ErrorProvider pictureErrorProvider;
+        private Bitmap myImage;
+        
+        public StudentManage()
+        {
+            InitializeComponent();
+
+            // Create and set the ErrorProvider for each data entry control.
+            nameErrorProvider = new ErrorProvider();
+            nameErrorProvider.SetIconAlignment(txtName, ErrorIconAlignment.MiddleRight);
+            nameErrorProvider.SetIconPadding(txtName, 2);
+            nameErrorProvider.BlinkRate = 200;
+            nameErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+
+            ageErrorProvider = new ErrorProvider();
+            ageErrorProvider.SetIconAlignment(nudAge, ErrorIconAlignment.MiddleRight);
+            ageErrorProvider.SetIconPadding(nudAge, 2);
+            ageErrorProvider.BlinkRate = 200;
+            ageErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+            emailErrorProvider = new ErrorProvider();
+            emailErrorProvider.SetIconAlignment(txtEmail, ErrorIconAlignment.MiddleRight);
+            emailErrorProvider.SetIconPadding(txtEmail, 2);
+            emailErrorProvider.BlinkRate = 200;
+            emailErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+            phoneErrorProvider = new ErrorProvider();
+            phoneErrorProvider.SetIconAlignment(txtPhone, ErrorIconAlignment.MiddleRight);
+            phoneErrorProvider.SetIconPadding(txtPhone, 2);
+            phoneErrorProvider.BlinkRate = 200;
+            phoneErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+            pictureErrorProvider = new ErrorProvider();
+            pictureErrorProvider.SetIconAlignment(pictAvatar, ErrorIconAlignment.MiddleRight);
+            pictureErrorProvider.SetIconPadding(pictAvatar, 2);
+            pictureErrorProvider.BlinkRate = 200;
+            pictureErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+        }
+
+        private void StudentManage_Load(object sender, EventArgs e)
+        {
+            loadStudents();
+            loadLanguage();
+        }
+
+        private void loadStudents()
+        {
+            string queryString = "SELECT StudentID, StudentName FROM Student";
+            SqlConnection conn = new SqlConnection(connectionString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(queryString, conn);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            cboStudents.DataSource = dataTable;
+            cboStudents.DisplayMember = "StudentName";
+            cboStudents.ValueMember = "StudentID";
+            conn.Close();
+        }
+
+        private void loadLanguage()
+        {
+            string queryString = "SELECT * FROM Language";
+            SqlConnection conn = new SqlConnection(connectionString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(queryString, conn);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            lstAvailabel.DataSource = dataTable;
+            lstAvailabel.DisplayMember = "LanguageName";
+            lstAvailabel.ValueMember = "LanguageID";
+            conn.Close();
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            if (cboStudents.Enabled)
+            {
+                cboStudents.Enabled = false;
+                txtName.Focus();
+                btnAddNew.Text = "Cancel";
+                btnUpdate.Text = "Add";
+                btnDelete.Enabled = false;
+            }
+            else
+            {
+                resetForm();
+            }
+        }
+
+        private void resetForm()
+        {
+            cboStudents.Enabled = true;
+            btnAddNew.Text = "Add New";
+            btnUpdate.Text = "Update";
+            btnDelete.Enabled = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                "Are you sure you want to exit?",
+                "Student Manager",
+                MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboStudents.Enabled)
+                {
+                    // todo
+                }
+                else
+                {
+                    string queryString = "INSERT INTO Student (StudentName, Age, Email, Phone, ImagePath) VALUES (@StudentName, @Age, @Email, @Phone, @ImagePath);";
+                    SqlConnection conn = new SqlConnection(connectionString);
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    SqlCommand command = new SqlCommand(queryString, conn);
+                    command.Parameters.Add("@StudentName", SqlDbType.NVarChar);
+                    command.Parameters["@StudentName"].Value = txtName.Text;
+                    command.Parameters.Add("@Age", SqlDbType.Int);
+                    command.Parameters["@Age"].Value = nudAge.Value;
+                    command.Parameters.Add("@Email", SqlDbType.NVarChar);
+                    command.Parameters["@Email"].Value = txtEmail.Text;
+                    command.Parameters.Add("@Phone", SqlDbType.NVarChar);
+                    command.Parameters["@Phone"].Value = txtPhone.Text;
+                    command.Parameters.Add("@ImagePath", SqlDbType.NVarChar);
+                    command.Parameters["@ImagePath"].Value = openFileDialog1.FileName;
+                    command.Connection = conn;
+                    Int32 rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show("RowsAffected {" + rowsAffected + "}");
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "Pictures";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string filePath = openFileDialog1.FileName;
+                ShowMyImage(filePath, 100, 100);
+            }
+        }
+
+        private void txtName_Validated(object sender, EventArgs e)
+        {
+            if (isNameValid())
+            {
+                nameErrorProvider.SetError(this.txtName, String.Empty);
+            }
+            else
+            {
+                nameErrorProvider.SetError(this.txtName, "Name is required.");
+            }
+        }
+
+        private bool isAgeInValid()
+        {
+            return (this.nudAge.Value == 0);
+        }
+
+        private bool isNameValid()
+        {
+            return (this.txtName.Text.Length > 0);
+        }
+
+        private bool isAgeTooYoung()
+        {
+            return (this.nudAge.Value < 18);
+        }
+
+        private bool isAgeTooOld()
+        {
+            return (this.nudAge.Value > 30);
+        }
+
+        private void nudAge_Validated(object sender, EventArgs e)
+        {
+            
+            if (isAgeInValid())
+            {
+                ageErrorProvider.SetError(this.nudAge, "Age is required.");
+            } else if (isAgeTooYoung())
+            {
+                ageErrorProvider.SetError(this.nudAge, "Age not old enough. Age range come from 18 - 30");
+            } else if (isAgeTooOld()) {
+                ageErrorProvider.SetError(this.nudAge, "Age is too old. Age range come from 18 - 30");
+            }
+            else
+            {
+                ageErrorProvider.SetError(this.nudAge, String.Empty);
+            }
+        }
+
+        private bool ValidEmailAddress(string emailAddress, out string errorMessage)
+        {
+            if (emailAddress.Length == 0)
+            {
+                errorMessage = "Email address is required.";
+                return false;
+            }
+
+            if (emailAddress.IndexOf("@") > -1)
+            {
+                if (emailAddress.IndexOf(".", emailAddress.IndexOf("@")) > emailAddress.IndexOf("@"))
+                {
+                    errorMessage = "";
+                    return true;
+                }
+            }
+
+            errorMessage = "Email address must be valid email address format.\n" + 
+                "For example 'someone@example.com'";
+            return false;
+
+        }
+
+        private bool isPhoneValid()
+        {
+            return (this.txtPhone.Text.Length > 0);
+        }
+
+        private bool phoneIsNumeric()
+        {
+            double num;
+            return double.TryParse(this.txtPhone.Text.Trim(), out num);
+        }
+
+        private void txtEmail_Validated(object sender, EventArgs e)
+        {
+            string errorMsg;
+            if (!ValidEmailAddress(this.txtEmail.Text, out errorMsg)) {
+                emailErrorProvider.SetError(this.txtEmail, errorMsg);
+            } else 
+            {
+                emailErrorProvider.SetError(this.txtEmail, String.Empty);
+            }
+        }
+
+        private void txtPhone_Validated(object sender, EventArgs e)
+        {
+            if (!isPhoneValid())
+            {
+
+                phoneErrorProvider.SetError(this.txtPhone, "Phone is required.");
+            } else if (!phoneIsNumeric())
+            {
+                phoneErrorProvider.SetError(this.txtPhone, "Invalid phone number.");
+            }
+            else
+            {
+                phoneErrorProvider.SetError(this.txtPhone, String.Empty);
+            }
+        }
+
+        private void pictAvatar_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowMyImage(String fileToDisplay, int xSize, int ySize)
+        {
+            if (myImage != null)
+            {
+                myImage.Dispose();
+            }
+            pictAvatar.SizeMode = PictureBoxSizeMode.StretchImage;
+            myImage = new Bitmap(fileToDisplay);
+            pictAvatar.ClientSize = new Size(xSize, ySize);
+            pictAvatar.Image = (Image)myImage;
+        }
+
+    }
+}
